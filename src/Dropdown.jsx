680aafb1c61b5d7f2b7e8ed4cb4389b57dae21b6
dikshantkanela <dikshant7156@gmail.com>
2025-04-16 22:18:36 +0530

    complete

--- src/Dropdown.jsx
+++ src/Dropdown.jsx
@@ -11,7 +11,7 @@ export default function DropdownMenu() {
         className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-3 py-2 text-center inline-flex items-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         type="button"
       >
-        Hire Talent
+        Services
         <svg
           className="w-2 h-2 ms-2"
           aria-hidden="true"
